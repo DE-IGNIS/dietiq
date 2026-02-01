aab65cfcde6dc264e600bbce9a7f2dba3dd0008b
DE-IGNIS <chauhansudarsh@gmail.com>
2026-02-01 10:24:12 +0530

    Content data and image assets

---
+++ src/constants/images.jsx
@@ -0,0 +1,19 @@
+import asthma from "../assets/images/asthma.jpg";
+import chikungunya from "../assets/images/chikungunya.jpg";
+import dengue from "../assets/images/dengue.jpg";
+import diabetes from "../assets/images/diabetes.jpeg";
+import hypertension from "../assets/images/hypertension.jpg";
+import malaria from "../assets/images/malaria.jpg";
+import tuberculosis from "../assets/images/tuberculosis.jpg";
+import typhoid from "../assets/images/typhoid.jpeg";
+
+export const card_images = {
+  asthma,
+  chikungunya,
+  dengue,
+  diabetes,
+  hypertension,
+  malaria,
+  tuberculosis,
+  typhoid,
+};
